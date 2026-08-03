ad3f18672d1f75c05efac31b925a7af706c4ea61
adammadill <adammadill1@gmail.com>
2026-08-04 00:48:43 +0100

    added middle finger. waiting on servos and elastic bands to be delivered. elastic bands should help with sideways movement of fingers and maybe with fingers twisting (hopefully)

--- src/main.cpp
+++ src/main.cpp
@@ -23,26 +23,36 @@
 
 Servo index_servo;
 Servo ring_servo;
+Servo middle_servo;
 
 const int index_ServoPin = 8; // orange
 const int ring_ServoPin = 9; // blue
+const int middle_ServoPin = 7; // green
 
 
 void setup() {
-  index_servo.attach(index_ServoPin);
-  index_servo.write(90);
-  ring_servo.attach(ring_ServoPin);
-  ring_servo.write(90);
+  // index_servo.attach(index_ServoPin);
+  // index_servo.write(0); // fingers up
+  // ring_servo.attach(ring_ServoPin);
+  // ring_servo.write(0);
+  middle_servo.attach(middle_ServoPin);
+  middle_servo.write(0);
 }
 
 void loop() {
-  index_servo.write(30);
-  ring_servo.write(30);
+  // index_servo.write(10);
+  // ring_servo.write(0);
+  // delay(1000);
+  // index_servo.write(90);
+  // ring_servo.write(90);
+  // delay(1000);
+  // index_servo.write(180);
+  // ring_servo.write(180);
+  // delay(1000);
+  middle_servo.write(0);
   delay(1000);
-  index_servo.write(90);
-  ring_servo.write(90);
+  middle_servo.write(90);
   delay(1000);
-  index_servo.write(170);
-  ring_servo.write(170);
+  middle_servo.write(180);
   delay(1000);
 }
