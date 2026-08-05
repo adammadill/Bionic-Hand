8f8dfdc0f05a0a0737db2b37b437ce3430599ccb
adammadill <adammadill1@gmail.com>
2026-08-05 15:12:49 +0100

    added selection for open/close. thought of an issue that when gripping something the servos will grind because they havent reached their angle. might have to order springs to fix it.

--- src/main.cpp
+++ src/main.cpp
@@ -33,37 +33,99 @@ const int middle_ServoPin = 7; // green
 const int pinky_ServoPin = 6; // yellow
 const int thumb1_ServoPin = 5; // blue
 
+// -----------------------------------------------------------------------------
+// TESTING 
+// -----------------------------------------------------------------------------
 
-void setup() {
+// void setup() {
   // index_servo.attach(index_ServoPin);
   // index_servo.write(0); // fingers up
   // ring_servo.attach(ring_ServoPin);
   // ring_servo.write(0);
   // middle_servo.attach(middle_ServoPin);
   // middle_servo.write(0);
-  pinky_servo.attach(pinky_ServoPin);
-  pinky_servo.write(0);
+  // pinky_servo.attach(pinky_ServoPin);
+  // pinky_servo.write(0);
   // thumb1_servo.attach(thumb1_ServoPin);
   // thumb1_servo.write(0);
-}
+// }
 
-void loop() {
+// void loop() {
   // index_servo.write(10);
   //ring_servo.write(0);
-  pinky_servo.write(0);
-  delay(1000);
+  // pinky_servo.write(0);
+  // delay(1000);
   // index_servo.write(90);
-  //ring_servo.write(90);
-  //delay(1000);
+  // ring_servo.write(90);
+  // delay(1000);
   // index_servo.write(180);
   //ring_servo.write(180);
-  pinky_servo.write(180);
-  delay(1000);
+  // pinky_servo.write(180);
+  // delay(1000);
   // middle_servo.write(0);
   // delay(1000);
   // middle_servo.write(90);
-  pinky_servo.write(60);
-  delay(1000);
+  //pinky_servo.write(60);
+  // delay(1000);
   // middle_servo.write(180);
   // delay(1000);
+// }
+
+// -----------------------------------------------------------------------------
+// MOVEMENTS 
+// -----------------------------------------------------------------------------
+
+// hand open hand close
+
+void handopen();
+
+void handclose();
+
+void setup() {
+
+  Serial.begin(9600);
+
+  Serial.println("Select an option by typing the number:");
+  Serial.println("1. Open hand");
+  Serial.println("2. Close hand");
+
+  while (Serial.available() == 0) {
+    // Wait for user input
+  }
+
+  int option = Serial.parseInt();
+
+  Serial.println("You selected option: " + option);
+
+  switch(option) {
+    case 1:
+      handopen();
+      break;
+    case 2:
+      handclose();
+      break;
+    default:
+      Serial.println("Invalid option.");
+      break;
+  }
+}
+
+void handopen() {
+  thumb1_servo.write(0);
+  index_servo.write(0);
+  middle_servo.write(0);
+  ring_servo.write(0);
+  pinky_servo.write(0);
+}
+
+void handclose() {
+  thumb1_servo.write(180);
+  index_servo.write(180);
+  middle_servo.write(180);
+  ring_servo.write(180);
+  pinky_servo.write(180);
+}
+
+void loop() {
+
 }
